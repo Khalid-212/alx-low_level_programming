2a1acd4e62657a227d320bd8535771f730b5d98d
khalid-212 <root@Inspirion-15.localdomain>
2022-03-16 23:54:14 +0300

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main (void){
-printf("_putchar");
 
-return(0);
+/**
+ * main -Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
 }
