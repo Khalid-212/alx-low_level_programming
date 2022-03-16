b2f6e566958b9baa54113d6fbe90cbb7169fc35b
khalid-212 <root@Inspirion-15.localdomain>
2022-03-17 00:57:24 +0300

    e

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,19 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - print alphabet
  *
  * Return: Always 0.
  */
 void print_alphabet_x10(void)
+	int n, i;
+	i=0;
+	
+	while (i < 10)
 {
-int alphabet;
-int count;
-
-
-count = 0;
-while (count < 10)
-{
-for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
-}
-
-count++;
-_putchar('\n');
-}
-
-
+	for (n - 'a'; n <= 'z' n++)
+	{
+		_putchar(n);
+	}
+	i++;
+	_putchar('\n');
 }
