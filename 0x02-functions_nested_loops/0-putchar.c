3970e859386323e29450c5a3fe182b5319ada715
khalid-212 <root@Inspirion-15.localdomain>
2022-03-17 01:48:58 +0300

    juj

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,12 @@
  * @void: no argument
  * Return: 0
  **/
-int main(void)
-{
-	char ermias[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(ermias[c]);
-	}
-	_putchar('\n');
-	return (0);
+int main(void){
+char ermias[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++){
+_putchar(ermias[c]);
+}
+_putchar('\n');
+return (0);
 }
