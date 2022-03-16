95a0949aa031c989a82db45eacf534e9c485c2da
khalid-212 <root@Inspirion-15.localdomain>
2022-03-17 01:04:16 +0300

    e

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,17 @@
  *
  * Return: Always 0.
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void){
 	int n, i;
 	i=0;
 	
 	while (i < 10)
-{
-	for (n - 'a'; n <= 'z' n++)
 	{
-		_putchar(n);
+		for (n - 'a'; n <= 'z' n++)
+		{
+			_putchar(n);
+		}
+		i++;
+		_putchar('\n');
 	}
-	i++;
-	_putchar('\n');
 }
